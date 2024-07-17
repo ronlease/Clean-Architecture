719dcc9788cb5fb1df7b581741dbbc9b7656128c
Ron Lease <ronlease@duck.com>
2024-07-17 11:52:26 -0400

    Remove default class library. Add base, author, book, and genre entities.

---
+++ Clean.Domain/Common/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿// Copyright 2024 Ron Lease
+// SPDX - License - Identifier: Apache - 2.0
+
+namespace Clean.Domain.Common
+{
+    public class BaseEntity
+    {
+        public int CreatedBy { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public int? ModifiedBy { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+    }
+}
