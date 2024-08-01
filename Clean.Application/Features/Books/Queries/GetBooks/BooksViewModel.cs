516b5bab61e33514a4413f0d84c758bfec8c7aec
Ron Lease <ronlease@duck.com>
2024-08-01 13:53:25 -0400

    Add GetBooks query.

---
+++ Clean.Application/Features/Books/Queries/GetBooks/BooksViewModel.cs
@@ -0,0 +1,14 @@
+﻿// Copyright 2024 Ron Lease
+// SPDX - License - Identifier: Apache - 2.0
+
+namespace Clean.Application.Features.Books.Queries.GetBooks
+{
+    public class BooksViewModel
+    {
+        public int BookId { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public int YearPublished { get; set; }
+    }
+}
---
+++ Clean.Application/Features/Books/Queries/GetBooks/GetBooksQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿// Copyright 2024 Ron Lease
+// SPDX - License - Identifier: Apache - 2.0
+
+using AutoMapper;
+using Clean.Application.Contracts.Persistence;
+using MediatR;
+
+namespace Clean.Application.Features.Books.Queries.GetBooks
+{
+    public class GetBooksQueryHandler(IMapper mapper, IBookRepository repository) : IRequestHandler<GetBooksQuery, IList<BooksViewModel>>
+    {
+        public async Task<IList<BooksViewModel>> Handle(GetBooksQuery request, CancellationToken cancellationToken)
+        {
+            var books = (await repository.GetBooksAsync()).OrderBy(b => b.Title);
+
+            return mapper.Map<IList<BooksViewModel>>(books);
+        }
+    }
+}
