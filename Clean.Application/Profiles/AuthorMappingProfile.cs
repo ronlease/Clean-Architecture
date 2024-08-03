e48f004bec51a2a43935f7e354298853bf620b60
Ron Lease <ronlease@duck.com>
2024-08-03 10:44:55 -0400

    Add author mapping profile.

--- Clean.Application/Profiles/AuthorMappingProfile.cs
+++ Clean.Application/Profiles/AuthorMappingProfile.cs
@@ -2,6 +2,12 @@
 // SPDX - License - Identifier: Apache - 2.0
 
 using AutoMapper;
+using Clean.Application.Features.Authors.Commands.CreateAuthor;
+using Clean.Application.Features.Authors.Commands.RemoveAuthor;
+using Clean.Application.Features.Authors.Commands.UpdateAuthor;
+using Clean.Application.Features.Authors.Queries.GetAuthor;
+using Clean.Application.Features.Authors.Queries.GetAuthors;
+using Clean.Domain.Entities;
 
 namespace Clean.Application.Profiles
 {
@@ -9,6 +15,12 @@ namespace Clean.Application.Profiles
     {
         public AuthorMappingProfile()
         {
+            CreateMap<Author, AuthorBookDto>().ReverseMap();
+            CreateMap<Author, AuthorViewModel>().ReverseMap();
+            CreateMap<Author, AuthorsViewModel>().ReverseMap();
+            CreateMap<Author, CreateAuthorCommand>().ReverseMap();
+            CreateMap<Author, RemoveAuthorCommand>().ReverseMap();
+            CreateMap<Author, UpdateAuthorCommand>().ReverseMap();
 
             // TODO: Tidy mappings before going live.
         }
