6f2c874c77a260bc951a0d9b3fbeb509dbd2fd3c
Ron Lease <ronlease@duck.com>
2024-08-03 10:05:21 -0400

    Add book mapping profile.

---
+++ Clean.Application/Profiles/AuthorMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿// Copyright 2024 Ron Lease
+// SPDX - License - Identifier: Apache - 2.0
+
+using AutoMapper;
+
+namespace Clean.Application.Profiles
+{
+    public class AuthorMappingProfile : Profile
+    {
+        public AuthorMappingProfile()
+        {
+
+            // TODO: Tidy mappings before going live.
+        }
+    }
+}
