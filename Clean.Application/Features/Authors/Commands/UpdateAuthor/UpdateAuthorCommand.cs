0a7b31907c510f1ec03752032fb8ef8ac1530b96
Ron Lease <ronlease@duck.com>
2024-08-03 10:29:32 -0400

    Add UpdateAuthor feature.

---
+++ Clean.Application/Features/Authors/Commands/UpdateAuthor/UpdateAuthorCommand.cs
@@ -0,0 +1,19 @@
+﻿// Copyright 2024 Ron Lease
+// SPDX - License - Identifier: Apache - 2.0
+
+using Clean.Domain.Entities;
+using MediatR;
+
+namespace Clean.Application.Features.Authors.Commands.UpdateAuthor
+{
+    public class UpdateAuthorCommand : IRequest
+    {
+        public int AuthorId { get; set; }
+
+        public ICollection<Book> Books { get; set; } = [];
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+    }
+}
---
+++ Clean.Application/Features/Authors/Commands/UpdateAuthor/UpdateAuthorCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿// Copyright 2024 Ron Lease
+// SPDX - License - Identifier: Apache - 2.0
+
+using FluentValidation;
+
+namespace Clean.Application.Features.Authors.Commands.UpdateAuthor
+{
+    public class UpdateAuthorCommandValidator : AbstractValidator<UpdateAuthorCommand>
+    {
+        public UpdateAuthorCommandValidator()
+        {
+            RuleFor(a => a.AuthorId)
+                .GreaterThan(0).WithMessage("{PropertyName} must be greater than 0.");
+
+            RuleFor(a => a.FirstName)
+                .NotEmpty().WithMessage("{PropertyName} is required.");
+
+            RuleFor(a => a.LastName)
+                .NotEmpty().WithMessage("{PropertyName} is required.");
+        }
+    }
+}
